f9fae1c23f1898c2d51b7c6b835f545877af3faa
hossein101 <hossein_lotg@yahoo.com>
2017-06-10 14:37:12 +0430

    Create api.lua

--- api.lua
+++ api.lua
@@ -1,15 +1,15 @@
 ﻿local URL = require "socket.url"
 local https = require "ssl.https"
 local serpent = require "serpent"
-local json = (loadfile "/home/bibak/INLINE/JSON.lua")()
-local token = '386343615:AAEdTNZP025yDWqUhBAdb7vW9oHVCLewdSk' --token
+local json = (loadfile "/home/ho3ein/INLINE/JSON.lua")()
+local token = '391973483:AAF8e1SqRHy5wCWwRStUkkEkMo-PEJC-GPA' --token
 local url = 'https://api.telegram.org/bot' .. token
 local offset = 0
 local redis = require('redis')
 local redis = redis.connect('127.0.0.1', 6379)
-local SUDO = 261764158
+local SUDO = 365225167
 function is_mod(chat,user)
-sudo = {261764158}
+sudo = {365225167}
   local var = false
   for v,_user in pairs(sudo) do
     if _user == user then
@@ -163,7 +163,7 @@ local function run()
           offset = msg.update_id + 1
           if msg.inline_query then
             local q = msg.inline_query
-						if q.from.id == 370725344 or q.from.id == 261764158 then
+						if q.from.id == 344981339 or q.from.id == 365225167 then
             if q.query:match('%d+') then
               local chat = '-'..q.query:match('%d+')
 							local function is_lock(chat,value)
